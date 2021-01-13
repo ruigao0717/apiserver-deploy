6b326ef2bce3c2c631e2608c80fbed354ee67882
RuiGao <ruigao0717@gmail.com>
2021-01-14 00:47:30 +1000

    init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const app = express();
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json()); // change body's content to js
+app.use(morgan("dev"));
+
+app.use("/v1", routes);
+
+
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`);
+});
